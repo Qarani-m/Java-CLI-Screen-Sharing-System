121d747eb773d39c9995519a04d07ab1cd30922b
martin <emqarani@gmail.com>
2025-05-11 09:22:03 +0300

    Update Message.java - Removed dead code from protocol

--- src/main/java/com/screenshare/common/Message.java
+++ src/main/java/com/screenshare/common/Message.java
@@ -60,3 +60,5 @@ public class Message implements Serializable {
 /* 2025-06-19 16:26:52: Modularized protocol logic */
 
 /* 2025-06-19 16:26:52: Enhanced thread safety */
+
+/* 2025-06-19 16:26:53: Improved object serialization logic */
