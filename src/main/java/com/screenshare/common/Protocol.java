9d00e6e89ed8db76c86bc8cfae2474a0ad75b49d
martin <emqarani@gmail.com>
2025-05-12 18:24:09 +0300

    Update Protocol.java - Thread pooling enhanced

--- src/main/java/com/screenshare/common/Protocol.java
+++ src/main/java/com/screenshare/common/Protocol.java
@@ -81,3 +81,5 @@ public class Protocol {
 /* 2025-06-19 16:26:53: Updated server configuration handling */
 
 /* 2025-06-19 16:26:54: Modularized protocol logic */
+
+/* 2025-06-19 16:26:54: Added null safety checks */
